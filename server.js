05c178de235d9f9fc641ae3e9ecbdb1acf39940a
Mohammad Salim <jd2474ev@gmail.com>
2015-06-29 17:25:05 -0400

    Added controllers, sets, use, catchcall, and database to server

--- server.js
+++ server.js
@@ -9,3 +9,53 @@ var express =  require('express')
     morgan  =   require('morgan'),
     mongoose =  require('mongoose'),
     session =   require('express-session');
+
+// SET
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+// USE
+// Need more explanation for resave and saveUnititialized
+server.use(session({
+  secret: "thisismysecret",
+  resave: true,
+  saveUninitialized: false
+}));
+
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+server.use(express.static('./public'));
+server.use(methodOverride('_method'));
+server.use(morgan('short'));
+server.use(expressLayouts);
+
+// ROUTES + CONTROLLERS
+
+server.get('/', function(req,res){
+  res.render('welcome')
+});
+
+// CATCHCALL ROUTES
+
+server.use(function(req,res){
+  res.send("This is not the web page you are looking for..")
+});
+
+// DATABASE + SERVER
+
+mongoose.connect('mongodb://localhost:27017/forums_app')
+var db = mongoose.connection;
+
+db.on('error', function(){
+  console.log("DeathStar shields are down.");
+});
+
+db.once('open', function(){
+  console.log("DeathStar shields are operational.");
+  server.listen(3000, function(){
+    console.log("DeathStar superlaser is primed and ready.")
+  });
+});
