44f93163c6f11a4a11ea6187a0543bfc3f94a01b
Mohammad Salim <jd2474ev@gmail.com>
2015-07-01 10:00:00 -0400

    Buttons are not working, able to post. Needs a lot of work

---
+++ public/scripts/script.js
@@ -0,0 +1,19 @@
+$('#homeButton').click(function() {
+    window.location.href = '/forums';
+    return false;
+});
+
+$('#loginButton').click(function() {
+    window.location.href = '';
+    return false;
+});
+
+$('#signupButton').click(function() {
+    window.location.href = '';
+    return false;
+});
+
+$('#newButton').click(function() {
+    window.location.href = '/new';
+    return false;
+});
